ecdbff111ae307eefac0b7c26ca5c9ed2d3f6422
Williams <wmejia.corbera@gmail.com>
2023-10-07 15:10:58 -0400

    started filtering input from user

--- django/recipes/forms.py
+++ django/recipes/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 from .models import recipe
 
+
 class RecipeForm(forms.ModelForm):
+    Title = forms.CharField(widget = forms.TextInput(attrs={"placeholder": "Your Title"}))
     class Meta:
         model = recipe
         fields = [
@@ -12,3 +14,11 @@ class RecipeForm(forms.ModelForm):
         'Ingredients',
         'Directions'
         ]
+    def clean_Title(self, *args, **kwargs):
+        title = self.cleaned_data.get('Title').split()
+        with open('BannedWords.txt') as file:
+            contents = file.read()
+            for word in title:
+                if word in contents:
+                    raise forms.ValidationError("Please do not use inappropriate language")
+        return " ".join(title)
